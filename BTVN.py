d27f8983a6c825e7267217c626143ac4880e2be9
HuanHy-creator <tranhuanhy2010@gmail.com>
2024-07-21 10:22:50 +0700

    Add files via upload
    BTVN 7/14

---
+++ BTVN.py
@@ -0,0 +1,22 @@
+cannang = float(input("Nhập cân nặng của bạn:"))
+chieucao = float(input("Nhập chiều cao của bạn:"))
+
+chieucaomet = chieucao/100
+bmi = cannang/(chieucaomet*2)
+
+if bmi < 16 :
+   print("Gầy cấp độ III")
+elif (bmi >= 16) & (bmi < 17):
+   print("Gầy cấp độ II")
+elif (bmi >=17) & (bmi < 18.5):
+   print("Gầy cấp độ I")
+elif (bmi >= 18.5) & (bmi < 25):
+   print("Bình thường")
+elif (bmi <= 25) & (bmi < 30):
+   print("Thừa cân")
+elif (bmi <= 30) & (bmi < 35):
+   print("Béo phì cấp độ I")
+elif (bmi <= 35) & (bmi < 40):
+   print("Béo phì cấp độ II")
+else:
+   print("Béo phì cấp độ III")
